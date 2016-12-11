7adbff03d1b4481b63159ded0aec2de9803bb1c0
Luke <stanford.luke.nz@gmail.com>
2016-12-12 10:38:05 +1300

    add slider control for rocket

---
+++ Assets/EastWallBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EastWallBehaviour : MonoBehaviour {
+
+
+	static float ZeroPos = 1.0f;
+	static float MaxPos = -1.0f;
+    static float progress = 0;
+	void Start () {
+		resetSliderPosToCenter ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = new Vector3(Mathf.Lerp (ZeroPos, MaxPos, progress),transform.localPosition.y,transform.localPosition.z);
+	}
+
+	public static void resetSliderPosToCenter(){
+		progress = MaxPos + ((ZeroPos - MaxPos) / 2.0f);
+	}
+	public static bool updateSliderPos(float delta){
+		delta /= 100.0f;
+		Debug.Log ("slider pos delta = " + delta);
+		if (progress+delta < MaxPos || progress + delta > ZeroPos ){
+			return false;
+		}
+		progress += delta;
+		return true;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("East wall collision Enter");
+	}
+	void OnTriggerExit(Collider other) {
+		Debug.Log("East wall collision Exit");
+	}
+	void OnTriggerStay(Collider other) {
+		Debug.Log("East wall collision Stay");
+	}
+}
