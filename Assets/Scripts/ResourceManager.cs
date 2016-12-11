921a7ff093a1f17dfa21c51d166185e48b2ee0ea
James <jamestron@gmail.com>
2016-12-12 10:52:07 +1300

    added resourceManager and rocket landing display is working

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceManager : MonoBehaviour {
+
+	public int population;
+	public int power;
+	public float timeTillLanding;
+	public string landingTimeText;
+	private float nextLandingTime;
+
+
+	// Use this for initialization
+	void Start () {
+		population = 120;
+		nextLandingTime = 120f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeTillLanding = nextLandingTime - Time.time;
+		landingTimeText = landingTimeTextFormat (timeTillLanding);
+		
+	}
+
+
+	string landingTimeTextFormat(float t)
+	{
+		int min = Mathf.FloorToInt(t/60);
+		int sec = Mathf.FloorToInt(t) - (min * 60);
+
+		return min + ":" + sec; 
+	}
+}
