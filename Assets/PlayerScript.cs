02797ed16e0fa27c9beccb353260466890a98f10
Luke <stanford.luke.nz@gmail.com>
2016-12-11 21:25:49 +1300

    cube is now a toggle switch

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -5,10 +5,9 @@ public class PlayerScript : MonoBehaviour {
 
 	void Start () {
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	void FixedUpdate () {
@@ -16,8 +15,21 @@ public class PlayerScript : MonoBehaviour {
 		Vector3 fwd = transform.TransformDirection(Vector3.forward);
 		Ray ray = new Ray(Camera.main.transform.position, fwd);
 		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit, 1.0F)){
+		if (Physics.Raycast(ray, out hit, 1.5F)){
 			Debug.Log("There is something in front of the object!" + hit.collider.name);
+			if (Input.GetMouseButtonDown(0)) {
+				
+				Component[] components = hit.collider.gameObject.GetComponents(typeof(NorthBoxBehaviour));
+				if (components.Length == 0) {
+					return;
+				}
+				NorthBoxBehaviour consoleButton = (NorthBoxBehaviour)components [0];
+				if (consoleButton.currentColor == Color.red) {
+					consoleButton.currentColor = Color.green;
+				} else {
+					consoleButton.currentColor = Color.red;
+				}
+			}
 		}
 	}
 }
