5e0eadc6b9d4509f8921a2caba2000bba4a76118
Luke Stanford <Luke Stanford>
2016-12-11 14:55:45 +1300

    add in ray castign for the main player

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+		if (Physics.Raycast (Camera.main.transform.position, fwd, 1)) { 
+			print ("There is something in front of the object!");
+		}
+
+	}
+}
