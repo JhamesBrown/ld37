8bd82a24f46e8b247cdac7e31d8565d74c239941
James <jamestron@gmail.com>
2016-12-13 10:49:02 +1300

    alarm light for reactor add and  working

---
+++ Assets/Scripts/Displays/Consoles/AlarmLight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour {
+
+	private Renderer renderer;
+	private ResourceManager resourceManager;
+
+	public GameObject AlarmAudioChild;
+
+	// Use this for initialization
+	void Start () {
+		resourceManager = GameObject.Find ("ResourceManager").GetComponent<ResourceManager>();
+		renderer = GetComponent<Renderer> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Material mat = renderer.material;
+		if (resourceManager.heat * 100f > 80f) {
+			
+
+			float emission = Mathf.PingPong (Time.time, 1.0f);
+			Color baseColor = Color.red; //Replace this with whatever you want for your base color at emission level '1'
+
+			Color finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
+			mat.SetColor ("_Color", Color.red);
+			mat.SetColor ("_EmissionColor", finalColor);
+		} else {
+			mat.SetColor ("_Color", Color.gray);
+			mat.SetColor("_EmissionColor", Color.black);
+		}
+
+
+	}
+}
