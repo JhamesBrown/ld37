97f528fc4a71bc2dc3d28a48be5bf44a8a34cfbd
James <jamestron@gmail.com>
2016-12-13 10:09:25 +1300

    added lights to rocket and dome consoles to indicate they are powered

---
+++ Assets/Scripts/Displays/Consoles/console_powerLight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class console_powerLight : MonoBehaviour {
+
+	private Renderer renderer;
+
+	public Material consolePowered;
+	public Material consoleUnPowered;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ResourceManager.currentPowerUse == ResourceManager.powerUse.PoweredRocketLander) {
+			renderer.material = consolePowered;
+		} else {
+			renderer.material = consoleUnPowered;
+		}
+	}	
+}
---
+++ Assets/Scripts/Displays/Consoles/console_powerLight_lifeSup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class console_powerLight_lifeSup : MonoBehaviour {
+
+	private Renderer renderer;
+
+	public Material consolePowered;
+	public Material consoleUnPowered;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (ResourceManager.currentPowerUse == ResourceManager.powerUse.PoweredLifeSupportCharger) {
+			renderer.material = consolePowered;
+		} else {
+			renderer.material = consoleUnPowered;
+		}
+	}
+}
