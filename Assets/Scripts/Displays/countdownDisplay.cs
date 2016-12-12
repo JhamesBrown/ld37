0c1a9f28f56fae7dccfad6f978c7ee8ee1597046
James <jamestron@gmail.com>
2016-12-12 23:19:34 +1300

    added Countdown Timer to back wall

---
+++ Assets/Scripts/Displays/countdownDisplay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class countdownDisplay : MonoBehaviour {
+
+
+	private float gameDuration = 300f; // This will be got from the gamemanager or resource manager depending on whats used for end game logic
+	private string displayText;
+	private float secondsRemaining;
+
+	void Update () {
+
+		secondsRemaining = gameDuration - Time.time;
+
+		displayText = "COUNTDOWN \n" + timeTextFormat(Mathf.Floor(secondsRemaining)); 
+		GetComponent<TextMesh> ().text = displayText;
+
+	}
+
+	string timeTextFormat(float secs)
+	{
+		int min = Mathf.FloorToInt(secs/60);
+		int sec = Mathf.FloorToInt(secs) - (min * 60);
+
+		return min + ":" + sec; 
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,6 +17,13 @@ public class GameManager : MonoBehaviour
 
 	void Update () 
 	{
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit ();
+		}
+		if (Input.GetKeyDown(KeyCode.F1)) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+		}
+
 		
 		gameIsFinished = checkFinish ();
 
